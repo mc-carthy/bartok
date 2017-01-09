a92e4bef2954bb064cbe6205cf1cb4a5c5c8e601
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-01-09 13:33:27 +0000

    Add visual representation of the hands, and also fan the hands to allow the player to see all hands, only the players cards are face up

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using UnityEngine;
+using System.Collections.Generic;
 using System.Linq;
 
 public enum PlayerType {
@@ -28,6 +29,7 @@ public class Player {
 
         // Add the card to the hand
         hand.Add (eCB);
+        FanHand ();
         return eCB;
     }
 
@@ -35,7 +37,60 @@ public class Player {
     public CardBartok RemoveCard (CardBartok cB)
     {
         hand.Remove (cB);
+        FanHand ();
         return cB;
+    }
+
+    public void FanHand ()
+    {
+        // startRot is the rotation about the Z axis of the first card
+        float startRot = 0;
+        startRot = handSlotDef.rot;
+        if (hand.Count > 1)
+        {
+            startRot += Bartok.S.handFanDegrees * (hand.Count - 1) / 2;
+        }
+        // Then each card is rotated handFanDegrees from that to fan the cards
+
+        // Move all the cards to their new positions
+        Vector3 pos;
+        float rot;
+        Quaternion rotQ;
+
+        for (int i = 0; i < hand.Count; i++)
+        {
+            // Rotate about the Z axis, also adds rotations of the different playerss hands
+            rot = startRot - Bartok.S.handFanDegrees * i;
+            // Quaternion representing the same rotation as rot
+            rotQ = Quaternion.Euler (0, 0, rot);
+
+            // pos is a Vector3 half a card height above (0, 0)
+            pos = Vector3.up * CardBartok.CARD_HEIGHT * 0.5f;
+
+            // Multiplying a Quaternion bt a Vector3 rotates that Vector3 by
+            // the rotation stored in the Quaternion. The result fives us a
+            // vector above [0, 0, 0] that has been rotated by rot degrees
+            pos = rotQ * pos;
+
+            // Add the base position of the players hand (which will be at the
+            // bottom center of the fan of the cards)
+            pos += handSlotDef.pos;
+            // This staggers the cards in the z direction, which isn't visible
+            // but which does keep their colliders from overlapping
+            pos.z = -0.5f * i;
+
+            // Set the localPosition and rotation of the ith card in the hand
+            hand [i].transform.localPosition = pos;
+            hand [i].transform.rotation = rotQ;
+            hand [i].state = CBState.hand;
+
+            // This uses a comparison operator to return true or false bool
+            // So if (type == PlayerType.human), hand [i].faceUp is set to true
+            hand [i].faceUp = (type == PlayerType.human);
+
+            // Set the SortOrder of the cards so that they overlap properly
+            hand [i].SetSortOrder (i * 4);
+        }
     }
 
 }
